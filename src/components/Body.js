f39676909691fdb3cc1e02c015c14923bcdb245a
itshmnt <hemantxs5@gmail.com>
2023-02-12 23:51:20 +0530

    Adding Search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,47 @@
+import { useState } from "react";
+
 import RestaurantCard from "./RestaurantCard";
-import { restaurant } from "../config";
+import { restaurantList } from "../config";
+
+function filterData(restaurants, searchText) {
+    const filteredData = restaurants.filter((restaurant) => 
+        restaurant.info.name.includes(searchText)
+    );
+    return filteredData;
+}
 
 const Body = () => {
+
+    const [searchText, setSearchText] = useState("");
+    const [restaurants, setRestaurants] = useState(restaurantList);
+
   return (
-    <div className="restaurant-list">
-        {
-            restaurant.map((outlet => {
-                return <RestaurantCard vendor={outlet.info} key = {outlet.info.id} />
-            }))
-        }
-    </div>
+    <>
+        <div className="search-container">
+            <input 
+                type="text"
+                className="search-input"
+                placeholder="Search"
+                value={searchText}
+                onChange={(e) => {
+                    setSearchText(e.target.value)
+                }}
+            />
+            <button className="search-btn" onClick={() => {
+                //need to filter the data
+                const data = filterData(restaurants, searchText);
+                // update the state - restaurant
+                setRestaurants(data);
+            }}>Search</button>
+        </div>
+        <div className="restaurant-list">
+            {
+                restaurants.map((outlet => {
+                    return <RestaurantCard vendor={outlet.info} key = {outlet.info.id} />
+                }))
+            }
+        </div>
+    </>
   )
 }
 
